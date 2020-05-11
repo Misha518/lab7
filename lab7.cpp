dd7cab4380efbe53f9e1e2f9ef8dd98a6833b593
Misha518 <62278075+Misha518@users.noreply.github.com>
2020-05-11 20:53:51 +0300

    Add files via upload

---
+++ lab7.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+double F(double x, double y){
+        return x*exp(-x)-2*y;
+}
+double dF(double x, double y){
+        return exp(-x)+x*exp(-x);
+}
+
+int main() {
+        double a=0; double b=2; double h=0.1;
+        double n=(b-a)/h;
+        double X[(int)n];
+        double Y1[(int)n];
+        double Y[(int)n];
+        double X1[(int)n];
+        double Y2[(int)n];
+        double Y3[(int)n];
+        ofstream fout;
+        ofstream kout;
+        fout.open("y.txt");
+        kout.open("y1.txt");
+        X[0]=a; Y[0]=0; 
+        for(int i=1; i<=n; i++){
+                X[i]=a+i*h;
+                Y1[i]=Y[i-1]+h*F(X[i-1],Y[i-1]);
+                Y[i]=Y[i-1]+h*(F(X[i-1],Y[i-1])+F(X[i],Y1[i]))/2.0;
+                
+        }
+        for(int i=1; i<=n; i++){
+                X1[i]=a+i*h;
+                Y2[i]=Y3[i-1]+h*dF(X[i-1],Y3[i-1]);
+                Y3[i]=Y3[i-1]+h*(dF(X[i-1],Y3[i-1])+dF(X[i],Y2[i]))/2.0;
+                
+        }
+        X[0]=a; Y[0]=0;
+        for(int i=0; i<=n; i++){
+                fout<<X[i]<<" "<<Y[i]<<endl;
+                kout<<X[i]<<" "<<Y3[i]<<endl;
+        }
+        fout.close();
+        kout.close();
+        for(int i=0; i<=n; i++){
+                cout << "X["<<i<<"]="<<X[i] <<" ";
+        }
+        cout << endl;
+        for(int i=0; i<=n; i++){
+                cout << "Y["<<i<<"]="<<Y[i] << " ";
+        }
+        cout<<endl;
+        for(int i=0; i<=n; i++){
+                cout << "dY["<<i<<"]="<<Y3[i] << " ";
+        }
+        cout<<endl;
+        system("python3 lab7.py");
+        return 0;
+}
